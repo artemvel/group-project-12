8a8e7cae522a40afe4dcd366bb1b4aed1e9f0f6e
Abraham <artemvel1995@gmail.com>
2021-06-30 21:56:52 -0400

    Finished my part like a BOSS

---
+++ src/test/java/ihsan/AbrahamWorking.java
@@ -0,0 +1,8 @@
+package ihsan;
+
+public class AbrahamWorking {
+    //I can teach u all curses in Russian
+    //I can teach u how to admire any "Natasha" u want
+    //Why Turkey became so rich?
+    //I can teach u all curses in Hebrew and Yidish as well
+}
