a6caf08ea4dac1fa93fd984fb612acc61d6fdb42
Abraham <artemvel1995@gmail.com>
2021-07-07 19:57:49 -0400

    Created My Folder to be #1 in your project

---
+++ src/test/java/ArtemKrais/NowIHaveMyOwnFolder.java
@@ -0,0 +1,8 @@
+package ArtemKrais;
+
+public class NowIHaveMyOwnFolder {
+    //Now I have my new folder
+    // I missed the class again
+    //Anyway, thank you Ihsan, u taught me gitHub in 10 minutes only
+    // Hope I didn't messed up here
+}
